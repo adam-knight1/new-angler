d43ccaa70531015e828c6549efc35059a4c91d77
Adam KnightJava17 <adamknightjava17@adamlaptop.local>
2023-12-09 13:54:11 -0500

    Initial project commit, DockerFile added

---
+++ src/main/java/com/springapps/newangler/NewanglerApplication.java
@@ -0,0 +1,13 @@
+package com.springapps.newangler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewanglerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewanglerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapps/newangler/NewanglerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapps.newangler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewanglerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
